edd68582cccfd181306e7112c70f5838b76913e8
imak02 <asbinkhanal222@gmail.com>
2022-06-15 09:13:25 +0545

    Added a comment by ak

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,6 +32,7 @@ function Navbar(){
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button className="btn btn-outline-success" type="submit">Search</button>
+          {/* Comment by Asbin Khanal */}
         </form>
       </div>
     </div>
