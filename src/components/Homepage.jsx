fac3d2478db17a25fb6165432e6b7acfd37599e8
imak02 <asbinkhanal222@gmail.com>
2022-06-16 19:58:30 +0545

    Added Carousel by AK

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Carousel from './Carousel'
+import "../styles/Carousel.css"
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+    <div><Carousel/></div>
+    <div><h1 style= {{color:'red'}}>Put your cards here</h1> </div>
+    </>
   )
 }
