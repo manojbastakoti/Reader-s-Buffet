465c26ae8f82316a74e96ff05782f70a1f6d2f18
Manoj Bastakoti <manojbastakoti252@gmail.com>
2022-06-17 20:54:03 +0545

    Make card details dynamic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,16 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ProductCard() {
+function ProductCard(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card className="overflow-hidden" id="product">
+      <Card.Img variant="top" src={props.img} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{props.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          Price:{props.price}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Buy</Button>
       </Card.Body>
     </Card>
   );
