33af5a10fd355f56df91f8bd94802f32b7ee3237
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-08-29 23:05:17 +0545

    Extract email from url in verification page

--- src/components/Verification.jsx
+++ src/components/Verification.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Card, Container, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import useQuery from "../hooks/useQuery";
 
 export default function Verification() {
+  const query = useQuery();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!query.get("email")) {
+      navigate("/login");
+      return;
+    }
+  }, []);
+
   return (
     <Container>
       <Card className="mx-auto p-3 my-4">
@@ -9,7 +21,8 @@ export default function Verification() {
           <Form.Text>
             <h1 className="text-center">Verification</h1>
             <p>
-              Please enter the verification code sent to your email address.
+              Please enter the verification code sent to your email address{" "}
+              <b>{query.get("email")}</b>
             </p>
 
             <Form.Group controlId="formBasicEmail" className="mb-2">
@@ -18,6 +31,7 @@ export default function Verification() {
                 type="number"
                 maxLength={6}
                 placeholder="Enter code"
+                autoFocus
               />
             </Form.Group>
             <Button className="w-100" type="submit">
