b079e7f16c5b35a033d3371c6afbaa9e2eb5fac2
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-19 15:50:24 +0545

    Show exchange tokens on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,31 @@ import { LinkContainer } from "react-router-bootstrap";
 import * as Icon from "react-bootstrap-icons";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/slices/authSlice";
+import { Badge, OverlayTrigger, Popover } from "react-bootstrap";
 
 function Appbar() {
   const dispatch = useDispatch();
-  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  const exchangeTokenPopover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Exchange Tokens</Popover.Header>
+      <Popover.Body>
+        {user?.exchangeTokenCount > 0 ? (
+          <>
+            You have {user?.exchangeTokenCount} exchange tokens. It means you
+            can borrow {user?.exchangeTokenCount} books from others.
+          </>
+        ) : (
+          <>You have no exchange tokens.</>
+        )}
+        <br />
+        To increase the token count, you can add more books that you have
+        already read.
+      </Popover.Body>
+    </Popover>
+  );
+
   return (
     <Navbar bg="light" expand="lg" sticky="top">
       <Container fluid>
@@ -62,11 +83,25 @@ function Appbar() {
             </Button>
           </Form>
 
-          <div className="d-flex gx-3 ms-4">
+          <div className="d-flex gap-2 ms-4">
             {isAuthenticated ? (
               <>
+                <OverlayTrigger
+                  trigger="click"
+                  placement="bottom"
+                  overlay={exchangeTokenPopover}
+                  rootClose
+                >
+                  <Badge
+                    bg="warning"
+                    className="text-black d-flex align-items-center gap-2"
+                    title="This represents the number of books you can borrow from others."
+                  >
+                    {user?.exchangeTokenCount} <Icon.PiggyBank size={20} />
+                  </Badge>
+                </OverlayTrigger>
                 <LinkContainer to="/profile">
-                  <Button variant="outline-dark" className="me-2">
+                  <Button variant="outline-dark" className="">
                     Profile
                   </Button>
                 </LinkContainer>
