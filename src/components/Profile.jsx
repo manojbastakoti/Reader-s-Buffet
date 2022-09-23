641b8fbe96f1f5fd6c61ced538cd041e512154e5
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-23 23:11:40 +0545

    Show exchanged books on profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,8 +12,9 @@ export default function Profile() {
   const dispatch = useDispatch();
 
   const getBooks = () => axios.get("/book/mine");
+  const getExchangedBooks = () => axios.get("/book/exchanged");
 
-  const [userResult, bookResult] = useQueries({
+  const [userResult, bookResult, exchangedBookResult] = useQueries({
     queries: [
       {
         queryKey: ["current-user", 1],
@@ -30,6 +31,18 @@ export default function Profile() {
       {
         queryKey: ["my-books", 2],
         queryFn: getBooks,
+        onSuccess: (data) => {
+          if (data.status === 200) {
+            // dispatch(setUser(data.data.data));
+          }
+        },
+        onError: (error) => {
+          console.log(error);
+        },
+      },
+      {
+        queryKey: ["my-exchanged-books", 3],
+        queryFn: getExchangedBooks,
         onSuccess: (data) => {
           if (data.status === 200) {
             // dispatch(setUser(data.data.data));
@@ -77,7 +90,7 @@ export default function Profile() {
             <p>{userResult.data.data.data.phone}</p>
           </Card>
         </Col>
-        <Col xs={9}>
+        <Col xs={9} className="d-flex flex-column gap-3">
           <Card className="shadow-sm p-3 w-100 h-auto">
             <div className="d-flex justify-content-between  align-items-center mb-3">
               <h4>My Books</h4>
@@ -110,6 +123,40 @@ export default function Profile() {
                       </b>
                     </LinkContainer>
                     to add books.
+                  </span>
+                </div>
+              )}
+            </Card>
+          </Card>
+          <Card className="shadow-sm p-3 w-100 h-auto">
+            <div className="d-flex justify-content-between  align-items-center mb-3">
+              <h4>Exchanged Books</h4>
+              <LinkContainer to="/add-book">
+                <Button variant="primary">Add Book</Button>
+              </LinkContainer>
+            </div>
+            <h6>You have obtained these books in exchange for your books.</h6>
+            <Card className="p-2 w-100 h-auto d-flex  flex-row gap-2 flex-wrap">
+              {exchangedBookResult?.data?.data?.data?.books.length !== 0 ? (
+                exchangedBookResult?.data?.data?.data?.books.map((book) => (
+                  <ProductCard
+                    key={book._id}
+                    book={book}
+                    title={book.title}
+                    price={book.price}
+                    img={process.env.REACT_APP_BASE_API + book.cover}
+                  />
+                ))
+              ) : (
+                <div>
+                  <span className="text-center">
+                    You don't have any exchanged books. <br />
+                    <LinkContainer to="/add-book">
+                      <b role="button" className="text-primary ">
+                        Click here{" "}
+                      </b>
+                    </LinkContainer>
+                    to obtain books in exchange for your books.
                   </span>
                 </div>
               )}
