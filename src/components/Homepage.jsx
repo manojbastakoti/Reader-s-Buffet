54133b743f8138c41bfc8a10202877b485b90cba
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-24 21:42:12 +0545

    Extract exchange section into separate component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import Carousel from "./Carousel";
 import "../styles/Carousel.css";
-import ExchangeCard from "./ExchangeCard";
-import ProductCard from "./Card";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { LinkContainer } from "react-router-bootstrap";
 import Loading from "./Loading";
+import ExchangeSection from "./ExchangeSection";
 
 export default function Homepage() {
-  const { data, isLoading, isError } = useQuery(["all-books"], async () =>
-    axios.get("/book/all")
-  );
+  // const { data, isLoading, isError } = useQuery(["all-books"], async () =>
+  //   axios.get("/book/all")
+  // );
 
-  const books = data?.data?.data?.books;
+  // const books = data?.data?.data?.books;
 
-  if (isLoading) return <Loading />;
-  if (isError) return <div>Something went wrong!</div>;
+  // if (isLoading) return <Loading />;
+  // if (isError) return <div>Something went wrong!</div>;
 
   return (
     <>
@@ -24,22 +20,16 @@ export default function Homepage() {
         <Carousel />
       </div>
 
-      <section className="d-flex flex-column gap-4 p-4 ">
-        <h3 className="text-center">Exchange</h3>
-        <div className="d-flex  flex-wrap gap-3  mt px-4">
-          {books?.map((book) => (
-            <ExchangeCard key={book._id} book={book} />
-          ))}
-        </div>
-      </section>
-      <section className="d-flex flex-column gap-4 p-4 border mt-5 ">
+      <ExchangeSection />
+
+      {/* <section className="d-flex flex-column gap-4 p-4 border mt-5 ">
         <h3 className="text-center">Buy</h3>
         <div className="d-flex  flex-wrap gap-3   px-4">
           {books?.map((book) => (
             <ProductCard key={book._id} book={book} />
           ))}
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
